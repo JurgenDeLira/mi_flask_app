ecf9ffc90c1d2cdb52d6e4bf3079646c553f2451
JurgenDeLira <arq.jorgedelira@gmail.com>
2024-11-10 21:07:38 -0600

    Proyecto con Flask

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://jorgedelira:******@localhost/mi_flask_app'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Aquí puedes añadir más configuraciones
